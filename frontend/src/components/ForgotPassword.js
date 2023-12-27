1a70d59b5bea8e13ff5ca2eb549ce2d576436a1b
Sachin <sachinbajaj0001@gmail.com>
2023-12-27 12:20:10 +0530

    Initial setup and packages added

---
+++ frontend/src/components/ForgotPassword.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import toast from 'react-hot-toast'
+
+const ForgotPassword = () => {
+
+
+  return (
+    <div>
+        ForgotPassword
+    </div>
+  )
+}
+
+export default ForgotPassword

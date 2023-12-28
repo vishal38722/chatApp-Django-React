6c3344850e6ea02ec9eecb92e09d623bd735e669
Sachin <sachinbajaj0001@gmail.com>
2023-12-28 13:22:58 +0530

    Fix: UI Bug

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import ChatBox from './ChatBox'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4 col-lg-3">
+          <div class="bg-primary text-light p-3">
+            <h4>Sidebar</h4>
+          </div>
+        </div>
+        <div class="col-md-8 col-lg-9">
+          <div class="bg-light p-3">
+            <h4>Chat Box</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
   )
 }
 

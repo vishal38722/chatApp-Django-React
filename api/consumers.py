ce5d7d9a41336f745acb6c4c8596dd179354e093
Rudransh Srivastava <84043023+RUDRANSH-hub@users.noreply.github.com>
2024-01-03 11:13:58 +0530

    Delete api directory

--- api/consumers.py
+++
@@ -1,5 +0,0 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
-class PersonalChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        print(" TESTING REDIS CHANNEL")
-        await self.accept()
--- api/route.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .consumers import PersonalChatConsumer
-websocket_urlpatterns=[
-    path("ws/api/",PersonalChatConsumer.as_asgi())
-]
